14749f5fbfcd6278aabee334bc99a0005614a1c9
henry <tarou.88@gmail.com>
2020-04-29 10:44:06 +0900

    add structs and impl List fns

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,35 @@ impl TodoItem {
     }
 }
 
+struct TodoList {
+    list: Vec<TodoItem>,
+}
+
+impl TodoList {
+    fn new() -> TodoList {
+        return TodoList { list: Vec::new() };
+    }
+
+    fn add_to_list(&mut self, name: String) {
+        let todo_item = TodoItem::new(name);
+        self.list.push(todo_item);
+    }
+
+    fn print(&self) {
+        for item in &self.list {
+            println!("[{}] - {}", item.completed, item.name);
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = args[1].clone();
-    let todo_item1 = TodoItem::new("Say hi".to_string());
-    let todo_item2 = TodoItem::new("Say bye".to_string());
-    let todo_list = vec![todo_item1, todo_item2];
+    let mut todo_list = TodoList::new();
+
+    todo_list.add_to_list("Say hi".to_string());
 
     if command == "get" {
-        for item in todo_list {
-            println!("[{}] - {}", item.completed, item.name)
-        }
+        todo_list.print();
     }
 }
