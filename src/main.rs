30f1544795a1f5139b2905aeb4521735fcf7d84c
henry <tarou.88@gmail.com>
2020-04-29 14:18:16 +0900

    add remove and done

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ struct TodoItem {
 
 impl TodoItem {
     fn new(name: String) -> TodoItem {
-        return TodoItem {
+        TodoItem {
             name: name,
             completed: ' ',
-        };
+        }
     }
 }
 
@@ -20,7 +20,7 @@ struct TodoList {
 
 impl TodoList {
     fn new() -> TodoList {
-        return TodoList { list: Vec::new() };
+        TodoList { list: Vec::new() }
     }
 
     fn add_to_list(&mut self, name: String) {
@@ -29,33 +29,55 @@ impl TodoList {
     }
 
     fn print(&self) {
-        for item in &self.list {
-            println!("[{}] - {}", item.completed, item.name);
+        for (index, item) in self.list.iter().enumerate() {
+            println!("{} [{}] - {}", index, item.completed, item.name);
         }
+    }
+
+    fn mark_done(&mut self, index: usize) {
+        if self.list[index].completed == ' ' {
+            self.list[index].completed = 'x';
+        } else {
+            self.list[index].completed = ' ';
+        }
+    }
+
+    fn remove_task(&mut self, index: usize) {
+        self.list.remove(index);
     }
 }
 
 enum Command {
     Get,
     Add(String),
+    Done(usize),
+    Remove(usize),
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let arguments: Vec<String> = env::args().collect();
     let mut todo_list = TodoList::new();
 
-    let command = match args[1].as_str() {
+    let command = match arguments[1].as_str() {
         "get" => Command::Get,
-        "add" => Command::Add(args[2].clone()),
-        _ => panic!("command required."),
+        "add" => Command::Add(arguments[2].clone()),
+        "done" => Command::Done(arguments[2].parse().expect("error converting to integer")),
+        "remove" => Command::Remove(arguments[2].parse().expect("error parsing for a remove")),
+        _ => panic!("you must provide an accepted command"),
     };
-
-    todo_list.add_to_list("Say hi".to_string());
 
     match command {
         Command::Get => todo_list.print(),
         Command::Add(task) => {
             todo_list.add_to_list(task);
+            todo_list.print();
+        }
+        Command::Done(index) => {
+            todo_list.mark_done(index);
+            todo_list.print();
+        }
+        Command::Remove(index) => {
+            todo_list.remove_task(index);
             todo_list.print();
         }
     }
