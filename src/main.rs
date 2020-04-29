a536809ff24de7c714712b921b29f2234250a519
henry <tarou.88@gmail.com>
2020-04-29 10:24:56 +0900

    impl TodoItem struct

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,21 @@ struct TodoItem {
     completed: char,
 }
 
+impl TodoItem {
+    fn new(name: String) -> TodoItem {
+        return TodoItem {
+            name: name,
+            completed: ' ',
+        };
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = args[1].clone();
-    let todo_item = TodoItem {
-        name: "Say hi".to_string(),
-        completed: ' ',
-    };
-    let todo_list = vec![todo_item];
+    let todo_item1 = TodoItem::new("Say hi".to_string());
+    let todo_item2 = TodoItem::new("Say bye".to_string());
+    let todo_list = vec![todo_item1, todo_item2];
 
     if command == "get" {
         for item in todo_list {
