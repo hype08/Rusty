9580a15836c7a411882844b41b6a5317fd6959d5
henry <tarou.88@gmail.com>
2020-04-29 10:16:28 +0900

    create TodoItem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
+struct TodoItem {
+    name: String,
+    completed: char,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let command = args[1].clone();
+    let todo_item = TodoItem {
+        name: "Say hi".to_string(),
+        completed: ' ',
+    };
+    let todo_list = vec![todo_item];
+
+    if command == "get" {
+        for item in todo_list {
+            println!("[{}] - {}", item.completed, item.name)
+        }
+    }
 }
